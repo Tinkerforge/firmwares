51adf7537ba0f376fe24db587f0667ed803c5ea5
Matthias Bolte <matthias@tinkerforge.com>
2021-10-06 16:12:56 +0200

    Fix stat mode check

--- update-latest.py
+++ update-latest.py
@@ -23,7 +23,7 @@ def main():
                 path = os.path.join(root, file_)
                 s = os.stat(path)
 
-                if stat.S_IMODE(s.st_mode) != 0o644:
+                if stat.S_IMODE(s.st_mode) != 0o664:
                     print('WARNING: unexpected mode:', path, oct(s.st_mode))
 
                 if re.match(r'^{0}_.*_firmware_latest.z?bin$'.format(category), file_) != None:
